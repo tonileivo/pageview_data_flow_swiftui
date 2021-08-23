18e96658e41a0f0e449412b17e1ff0f763bd97b6
Toni Leivo <tonileivo92@gmail.com>
2021-08-23 09:24:39 +0300

    Project created

---
+++ pageview_data_flow/pageview_data_flow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pageview_data_flow
+//
+//  Created by Toni Leivo on 23.8.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ pageview_data_flow/pageview_data_flow/pageview_data_flowApp.swift
@@ -0,0 +1,17 @@
+//
+//  pageview_data_flowApp.swift
+//  pageview_data_flow
+//
+//  Created by Toni Leivo on 23.8.2021.
+//
+
+import SwiftUI
+
+@main
+struct pageview_data_flowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
